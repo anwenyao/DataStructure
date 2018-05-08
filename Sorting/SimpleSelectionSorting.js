056c11ac1aad649f6c9c593bf900b0ab54e3a7fc
anwenyao <anwenyao1997@foxmail.com>
2018-05-08 21:42:50 +0800

    增加简单选择排序

---
+++ Sorting/SimpleSelectionSorting.js
@@ -0,0 +1,21 @@
+'use strict'
+var arr = [2,6,56,96,12,46,25,36,45,69];
+var arr1 = [894,23,5698,754,325,695,2,36,69];
+console.log(arr1);
+simpleSelection(arr1);
+console.log(arr1);
+function simpleSelection(arr){
+	let index;
+	let temp;
+	for(let i = 0;i < arr.length;++i){
+		index = i;
+		for(let j = i + 1;j < arr.length;++j){
+			if(arr[index] > arr[j]){
+				index = j;	//记录最小值的下标
+			}	
+		}
+		temp = arr[i];
+		arr[i] = arr[index];
+		arr[index] = temp;
+	}
+}
